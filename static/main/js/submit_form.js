a0156ea7c4aaea235a8ae301d11eb355cf0fccf6
dalvtalub <vlad.bulat.65@mail.ru>
2021-03-04 19:13:45 +0300

    updated success function

--- static/main/js/submit_form.js
+++ static/main/js/submit_form.js
@@ -1,23 +1,31 @@
 $(document).ready(function () {
-    $('[name=send]').click(function (e) {
-        e.preventDefault();
+    $('[name=send]').click(function () {
+        // e.preventDefault();
         const team = $('[id=team_input]').val();
+        var csrf = $('[name=csrfmiddlewaretoken]').val();
         if (team === "") {
 
         } else {
             $.ajax({
-                cache: false,
-                headers: {
-                    'X-CSRF-TOKEN': $('[name=csrfmiddlewaretoken]')
-                    'Content-Type': 'application/json',
-                },
+                cache: 'true',
                 type: 'post',
                 url: '/',
-                data: {'team': team},
-                success: function (data) {
-
+                data: {
+                    team: team,
+                    csrfmiddlewaretoken: csrf,
                 },
-                dataType: 'json',
+                success: function (data) {
+                    let matches_str = ''
+                    for (let res of data.result) {
+                        let team1 = res.team_name1;
+                        let team2 = res.team_name2;
+                        let time = res.time;
+                        time = time.substring(0, time.length - 1);
+                        matches_str += '<h2 class="text"><a href="' + res.url + '" class="text matches"> ' + time + '  ' + team1
+                            + '-' + team2 + '</a></h2>'
+                        $('#post-text').html('<h2 class="article_text">'+ matches_str + '</h2>');
+                    }
+                },
             });
         }
     });
