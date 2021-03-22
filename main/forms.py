6cac4151b04b8dfefeb86641bf0845ab453e4325
dalvtalub <vlad.bulat.65@mail.ru>
2021-03-22 12:06:39 +0300

    deleted MatchesForm

--- main/forms.py
+++ main/forms.py
@@ -1,31 +1,10 @@
-from .models import Matches, Articles
-from django.forms import ModelForm, TextInput, DateTimeInput
+from .models import Articles
+from django.forms import ModelForm, DateTimeInput
 from django import forms
 
 
-# class MatchesForm(ModelForm):  # Create a form for match introduction data
-#     class Meta:  # Form settings
-#         model = Matches
-#         fields = ['team1', 'team2', 'time']
-#         widgets = {
-#             'team1': TextInput(attrs={
-#                 'class': 'form-control',
-#                 'placeholder': 'First team',
-#             }),
-#             'team2': TextInput(attrs={
-#                 'class': 'form-control',
-#                 'placeholder': 'Second team',
-#             }),
-#             'time': DateTimeInput(attrs=({
-#                 'type': 'datetime-local',
-#                 'format': '%d/%m/%Y %H:%M',
-#                 'class': 'form-control',
-#                 'placeholder': 'Date and time of match',
-#             })),
-#         }
-
-
 class ArticlesForm(ModelForm):
+    """Add form of Articles"""
     class Meta:
         model = Articles
         fields = ['text', 'time']
