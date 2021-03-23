53fe488d52d65c4d5d9e45f9da723fbd7d9c4723
dalvtalub <vlad.bulat.65@mail.ru>
2021-03-23 11:48:08 +0300

    changed redirect

--- add_article/views.py
+++ add_article/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from main.forms import ArticlesForm
-from main.models import Articles
 
 
 def add_article(request):
@@ -10,7 +9,7 @@ def add_article(request):
         form = ArticlesForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('/')  # after completing the addition - go to the galvanized page
+            return redirect('/articles')  # after completing the addition - go to the galvanized page
         else:
             error = 'Error form'
     # if request.method == 'POST':
@@ -30,26 +29,3 @@ def add_article(request):
         'error': error,
     }
     return render(request, 'add_article/add_article.html', context)
-
-
-# def add_match(request):
-#     error = ''
-#     if request.method == 'POST':
-#         form = MatchesForm(request.POST)
-#         team1 = request.POST['team1']
-#         team2 = request.POST['team2']
-#         if Teams.objects.filter(team=team1).exists() and Teams.objects.filter(team=team2).exists():
-#             if form.is_valid():
-#                 form.save()
-#                 return redirect('/')  # after completing the addition - go to the galvanized page
-#             else:
-#                 error = 'Error form'
-#         else:
-#             error = 'One of these teams is not exist'
-#
-#     form = MatchesForm()
-#     context = {
-#         'form': form,
-#         'error': error,
-#     }
-#     return render(request, 'add_article/add_article.html', context)
